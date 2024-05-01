ef7776c132390a345036a01a98be87df49d3922f
Franco RATOVOSON <jean.franco.ratovoson@gmail.com>
2024-05-01 12:52:45 +0300

    add selection

--- packages/use-list/src/use-list.ts
+++ packages/use-list/src/use-list.ts
@@ -1,8 +1,30 @@
-export function useList<T>(data: Array<T>) {
+import React from 'react'
+
+type UseListOptions<T, U> = {
+  getId: (element: T) => U
+}
+
+export function useList<T, U>(data: Array<T>, { getId }: UseListOptions<T, U>) {
+  const [selection, setSelection] = React.useState<Set<U>>(new Set([]))
+
+  const toogleSelection = React.useCallback(
+    (item: T, state?: boolean) =>
+      setSelection(currentSelection => {
+        const itemId = getId(item)
+        const selectionList = new Set(currentSelection)
+        if (state || !selectionList.has(itemId)) selectionList.add(itemId)
+        else selectionList.delete(itemId)
+        return new Set(selectionList)
+      }),
+    [getId]
+  )
+
   return {
     list: data
       .filter(() => true)
       .sort(() => 0)
-      .slice()
+      .slice(),
+    selection,
+    toogleSelection
   }
 }
