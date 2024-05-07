3046301f6fc9738c420ace29f0d84cc498a100e2
Franco RATOVOSON <jean.franco.ratovoson@gmail.com>
2024-05-07 19:16:39 +0300

    feat: set features optional

--- packages/use-list/src/use-list.ts
+++ packages/use-list/src/use-list.ts
@@ -1,6 +1,8 @@
 import { useListPagination } from './pagination'
 import { useListSelection } from './selection'
 
+type AtLeastOne<T, U = { [K in keyof T]: Pick<T, K> }> = Partial<T> & U[keyof U]
+
 type UseListOptions<T, U> = {
   defaultPageSize: number
   filterFn: (element: T) => boolean
@@ -8,21 +10,23 @@ type UseListOptions<T, U> = {
   sortFn: (elementA: T, elementB: T) => number
 }
 
+type UseListParamsType<T, U> = AtLeastOne<UseListOptions<T, U>>
+
 export function useList<T, U>(
   dataList: Array<T>,
-  { defaultPageSize, filterFn, getId, sortFn }: UseListOptions<T, U>
+  { defaultPageSize, filterFn, getId, sortFn }: UseListParamsType<T, U>
 ) {
-  const data = dataList.filter(filterFn)
-  const selection = useListSelection(data, getId)
+  const data = dataList.filter(filterFn ?? (() => true))
+  const selection = useListSelection(data, getId ?? (() => null))
   const { index, pageSize, ...pagination } = useListPagination(
     data,
-    defaultPageSize
+    defaultPageSize || 5
   )
 
   return {
-    list: data.sort(sortFn).slice(index, index + pageSize),
-    pageSize,
-    ...pagination,
-    ...selection
+    list: data.sort(sortFn || (() => 0)).slice(index, index + pageSize),
+    ...(defaultPageSize && { pageSize }),
+    ...(defaultPageSize && pagination),
+    ...(getId && selection)
   }
 }
